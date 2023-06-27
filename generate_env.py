a8dfa426ac10d0164a2e49dc7515e390b0898ddf
eric <ericdominguezm@gmail.com>
2023-06-28 01:14:57 +0200

    added generate_env.py

---
+++ generate_env.py
@@ -0,0 +1,23 @@
+""" This module requests certain tokens from the user and writes them to a new .env file """
+
+import os
+
+def main():
+    """Requests the tokens from the user and generates the .env file."""
+    # Names of the tokens required for this application
+    tokens = ['OPENAI_API_KEY', 'TELEGRAM_API_KEY', 'DEEPL_API_KEY']
+
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Combine the directory with the .env filename
+    env_path = os.path.join(script_dir, 'app/.env')
+
+    with open(env_path, 'w', encoding="utf-8") as _f:
+        for token in tokens:
+            # Request each token from the user and write it to the .env file
+            token_value = input(f'Please enter your {token}: ')
+            _f.write(f'{token}={token_value}\n')
+
+if __name__ == "__main__":
+    main()
